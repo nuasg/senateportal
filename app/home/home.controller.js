5af9a77e3b1a4e71d6b71db265ed76366463d192
Philip Lan <philiplan2017@u.northwestern.edu>
2016-07-09 16:41:40 -0500

    last before refactoring

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -2,21 +2,21 @@
 	angular.module("senator")
 	.controller("HomeController",["$scope", "$state", "$http", "$cookies", "DataFactory", function($scope, $state, $http, $cookies, DataFactory){
 		$scope.submit = function(){
-			// if ($scope.role === "Senator") {
-			// 	$state.go("senate");
-			// } else {
-			// 	$state.go("admin");
-			// }
-			FB.login(function(response){
-				if (response.status === 'connected') {
-	        document.getElementById('status').innerHTML = 'We are logged in';
-	        console.log("YES");
-	      } else if (response.status === 'not_authorized') {
-	        document.getElementById('status').innerHTML = 'Please log into this app.';
-	      } else {
-	        document.getElementById('status').innerHTML = 'Please log into Facebook.';
-	      }
-			});
+			if ($scope.role === "Senator") {
+				$state.go("senate");
+			} else {
+				$state.go("admin");
+			}
+			// FB.login(function(response){
+			// 	if (response.status === 'connected') {
+	  //       document.getElementById('status').innerHTML = 'We are logged in';
+	  //       console.log("YES");
+	  //     } else if (response.status === 'not_authorized') {
+	  //       document.getElementById('status').innerHTML = 'Please log into this app.';
+	  //     } else {
+	  //       document.getElementById('status').innerHTML = 'Please log into Facebook.';
+	  //     }
+			// });
 		}
 	}]);
 }());
