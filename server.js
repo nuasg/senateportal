afcd4371b622eab5257ae6452b5d6f98370c49ed
Philip Lan <philiplan2017@u.northwestern.edu>
2016-08-28 21:54:02 -0500

    added cookie middleware

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
-var express = require('express');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var path = require('path');
-var passport = require('passport');
-var flash = require('connect-flash');
-var session = require('express-session');
-var morgan = require('morgan');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const morgan = require('morgan');
 
-var app = express();
-var userController = require("./server/controller/user.controller");
-var documentController = require("./server/controller/document.controller");
-var termController = require("./server/controller/term.controller");
+const app = express();
+const userController = require("./server/controller/user.controller");
+const documentController = require("./server/controller/document.controller");
+const termController = require("./server/controller/term.controller");
 // Databases
-var promise = require('bluebird');
+const promise = require('bluebird');
 mongoose.Promise = promise;
 
-var dbURI = process.env.SENATOR_DB;
-var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },  
+const dbURI = process.env.SENATOR_DB;
+const options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },  
                 replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } }}; 
 mongoose.connect(dbURI, options);
 
@@ -47,12 +48,25 @@ app.use(session({
     secret: process.env.SENATOR_SECRET,
     name: 'asg-senate',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store: new MongoStore({ 
+        mongooseConnection: mongoose.connection,
+        ttl: 14 * 24 * 60 * 60
+    })
 }));
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 require('./server/config/passport')(passport);
+
+// Middleware
+const checkCookie = (req, res, next) => {
+	if (req.session.passport) {
+		next();
+	} else {
+		res.sendStatus(500);
+	}
+}
 
 // routing
 require('./routes.js')(app, passport);
@@ -62,20 +76,20 @@ app.get('/workaround', function(req,res){
 });
 
 // Backend API Routes
-app.get("/api/document/:id", documentController.getDocumentId);
-app.get("/api/document/date/:date", documentController.getDocumentByWeek);
+app.get("/api/document/:id", checkCookie, documentController.getDocumentId);
+app.get("/api/document/date/:date", checkCookie, documentController.getDocumentByWeek);
 app.post("/api/document", documentController.addDocument);
-app.get("/api/document", documentController.findAllDocuments);
+app.get("/api/document", checkCookie, documentController.findAllDocuments);
 app.delete("/api/document", documentController.deleteDocument);
 app.put("/api/document", documentController.updateDocument);
-app.get("/api/document/:start/:end", documentController.getdocumentByDateRange);
+app.get("/api/document/:start/:end", checkCookie, documentController.getdocumentByDateRange);
 // Users
 // app.get("/api/user", userController.authenticate, userController.getAllUsers);
 app.post("/api/user", userController.addUser);
-app.get("/api/user", userController.getUsers);
+app.get("/api/user", checkCookie, userController.getUsers);
 app.put("/api/user", userController.updateUser);
 app.delete("/api/user", userController.deleteUser);
 // Terms
-app.get("/api/terms/:date", termController.getTerms);
+app.get("/api/terms/:date", checkCookie, termController.getTerms);
 
 app.listen("5004");
