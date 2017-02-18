2de07d367fad57cdf9e8ba33f1f9fc012bc67537
Philip Lan <philiplan2017@u.northwestern.edu>
2017-02-18 16:17:31 -0600

    ui beginnings for attendence

--- app/admin/admin.attendence.controller.js
+++ app/admin/admin.attendence.controller.js
@@ -2,11 +2,16 @@
 	angular.module("senator")
 	.controller("AdminAttendenceController",["$scope", "$state", "$http", function($scope, $state, $http){
 		'use strict';
-        $http.get("/senate/api/user").success(
-            function(data) {
-                $scope.users = data;
-            }
-        );
-        
+        $scope.getData = function () {
+            $http.get("/senate/api/user").success(function(data) {
+                $scope.users = data.filter(function(item){
+                    if (item.role == "Senator" && item.firstName && item.active) {
+                        item.present = false;
+                        item.marked = false;
+                        return item;
+                    }
+                });
+            });
+        }
 	}])
 }());
